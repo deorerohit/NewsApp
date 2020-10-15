b85a37d8743bb43a909887ade4be24544f2b8ec1
Rohit Deore <deorerohit18@gmail.com>
2020-10-15 19:30:05 +0530

    imporved ui

---
+++ app/src/main/java/dev/deorerohit/newsapp/RecyclerAdapter.java
@@ -0,0 +1,101 @@
+package dev.deorerohit.newsapp;
+
+import android.icu.text.CaseMap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dev.deorerohit.newsapp.Activity.MainActivity;
+import dev.deorerohit.newsapp.Models.Article;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.DataHolderClass> {
+
+    List<Article> articleList = new ArrayList<>();
+    MainActivity mainActivity;
+
+    public RecyclerAdapter(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerAdapter.DataHolderClass onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_recycler_layout,parent, false);
+        return new DataHolderClass(view);
+    }
+
+
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerAdapter.DataHolderClass holder, int position) {
+        Article article = articleList.get(position);
+
+        holder.newsTitle_textview.setText(article.getTitle());
+        holder.newsFrom_textview.setText(article.getAuthor());
+
+        if(article.getUrlToImage()!= null) {
+            Glide.with(mainActivity)
+                    .load(article.getUrlToImage())
+                    .thumbnail(0.1f)
+                    .override(250,250)
+                    .centerCrop()
+                    .into(holder.thumbnail_imageview);
+
+        }
+        else {
+            Glide.with(mainActivity)
+                    .load(R.drawable.news_thumbnail)
+                    .thumbnail(0.1f)
+                    .override(250,250)
+                    .centerCrop()
+                    .into(holder.thumbnail_imageview);
+        }
+
+
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return articleList.size();
+    }
+
+    public void setNewsList(List<Article> articleList) {
+        this.articleList = articleList;
+        notifyDataSetChanged();
+    }
+
+
+
+
+    public class DataHolderClass extends RecyclerView.ViewHolder {
+
+        private TextView newsTitle_textview;
+        private TextView newsFrom_textview;
+        private ImageView thumbnail_imageview;
+
+
+
+        public DataHolderClass(@NonNull View itemView) {
+            super(itemView);
+
+            newsFrom_textview = itemView.findViewById(R.id.newsFrom_textview);
+            newsTitle_textview = itemView.findViewById(R.id.newsTitle_textview);
+            thumbnail_imageview = itemView.findViewById(R.id.thumbnail_imageview);
+        }
+
+
+    }
+}
